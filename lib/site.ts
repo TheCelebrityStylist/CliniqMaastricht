43cfffbf67d663621aadcd168b641f820317d3c6
TheCelebrityStylist <elkejaspers.eac@gmail.com>
2026-07-09 13:42:01 +0200

    Apply SEO site description update

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   name: 'CLINIQ Maastricht',
   url: 'https://www.cliniqmaastricht.nl',
-  description: 'Club, events en workshops in Maastricht.',
+  description: 'Uitgaan in Maastricht? CLINIQ is dé club aan de Platielstraat. Clubavonden, cocktail workshops en ruimte voor besloten feesten. Open do, vr & za.',
   address: { street: 'Platielstraat 9A', postalCode: '6211 GV', city: 'Maastricht', country: 'NL' },
   phone: '+31 6 12530987',
   email: 'contact@cafecliniq.com',
